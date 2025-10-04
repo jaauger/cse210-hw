8709d0c7be750acca4c7ebed70a45535912b0247
jaauger <jaauger@yahoo.com>
2025-10-04 14:35:39 -0600

    Learning Activity: Polymorphism

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Net.Http.Headers;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square1 = new Square("red", 3);
+        Rectangle rectangle1 = new Rectangle("Blue", 4, 5);
+        Circle circle1 = new Circle("Green", 6);
+
+        shapes.Add(square1);
+        shapes.Add(rectangle1);
+        shapes.Add(circle1);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
